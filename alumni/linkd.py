f26286308df8b3d947310177b84ce041e9f097b5
jdelpego <jdelpego2@gmail.com>
2025-04-25 22:57:17 -0700

    Set it up to work with both apollo and linkd

--- alumni/linkd.py
+++ alumni/linkd.py
@@ -7,7 +7,7 @@ api_key = os.getenv("LINKD_API_KEY")
 if not api_key:
     raise ValueError("LINKD_API_KEY is not set in the .env file.")
 
-def send_request(query, limit=30):
+def get_contacts(query, limit=30):
     url = "https://search.linkd.inc/api/search/users"
     headers = {
         "Authorization": f"Bearer {api_key}",
