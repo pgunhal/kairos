f324085bd6ef2f2b593c03f37731b015fbea171b
jdelpego <jdelpego2@gmail.com>
2025-04-25 20:29:36 -0700

    Structure for local API Keys and .env

--- alumni/linkd.py
+++ alumni/linkd.py
@@ -1,9 +1,15 @@
 import asyncio 
 import websockets
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 async def connect_to_websocket():
     uri = "wss://search.linkd.inc/api/search/ws"
-    api_key = ''
+    api_key = os.getenv("LINKD_API_KEY")
+    if not api_key:
+        raise ValueError("API_KEY is not set in the .env file.")
     headers = {"Authorization": f"Bearer {api_key}"}
     websocket = await websockets.connect(uri, extra_headers=headers)
     print("Connected to WebSocket")
